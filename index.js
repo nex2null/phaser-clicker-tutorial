94551b0a56ce0c054c1b91cadc9c0074b12abd6e
nex2null <nex2null@gmail.com>
2021-05-16 09:00:10 -0400

    Adding boilerplate

---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
+    useContentSize: true,
+    autoHideMenuBar: true
+  });
+  win.setMenu(null);
+  win.loadFile('dist/index.html');
+}
+
+app.whenReady().then(createWindow);
